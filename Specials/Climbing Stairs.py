45d911fc8d6df0086b1becc3236ee4fc701b7f76
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-05-08 18:01:32 +0530

    Create Climbing Stairs.py

---
+++ Specials/Climbing Stairs.py
@@ -0,0 +1,11 @@
+def climbing_stairs(n):
+    if n <= 2:
+        return n
+    a = 1
+    b = 2
+    for i in range(3,n+1):
+        a,b = b,a+b
+    return b
+
+n = int(input("Enter no.of steps : "))
+print(climbing_stairs(n))
