b027aeb3bf2a2b5cbfb6cf18ec98ccb86fced861
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-05-27 18:48:09 +0530

    Update Sqrt.py

--- Basic/Sqrt.py
+++ Basic/Sqrt.py
@@ -1,11 +1,13 @@
 def sqrt(a):
   if a < 2:
     return a
+ 
   low = 1
   high = a
 
   while low <= high:
     mid = (low + high) // 2
+   
     if mid * mid == a:
       return mid
     elif mid * mid < a:
